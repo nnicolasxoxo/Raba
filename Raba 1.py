65ee167d959b686c43f2d85155b3da0c393412ef
Juan Nicolás Barreto Parra <jnbarretop@correo.udistrital.edu.co>
2022-07-30 15:03:55 -0500

    nuevo commit

--- Raba 1.py
+++ Raba 1.py
@@ -1,6 +1,29 @@
-from turtle import width
 import pygame
 import os
 width, Height = 900,500
 Ventana= pygame.display.set_mode((width, Height))
 pygame.display.set_caption("Raba")
+
+Blanco= (255,255,255)
+Negro= (0, 0, 0)
+Red= (255, 255, 0)
+yellow= (255, 255, 0)
+Fotogramas=60
+
+def main():
+    clock= pygame.time.Clock()
+    run = True
+
+    while run:
+
+        clock.tick(Fotogramas)
+
+        for event in pygame.event.get():
+           if event.type== pygame.Quit:
+              run=False
+              pygame.quit()
+    main()
+if __name__ == "__main__":
+    main()
+
+
