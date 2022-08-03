66cdc2c80dc8311c86e419f4c8a7dbfa1eb103ab
Juan Nicolás Barreto Parra <jnbarretop@correo.udistrital.edu.co>
2022-08-03 17:53:08 -0500

    nuevo commit

--- Raba.py
+++ Raba.py
@@ -1,2 +1,12 @@
 #Aqui voy a crear el lugar del juego llamado raba
- 
+import pygame
+from pygame.locals import * 
+import sys
+import os
+
+pygame.init()
+pantalla= pygame.display.set_mode((1000,600))
+
+fondo=pygame.image.load("imagenes/fondo.jpg")
+pantalla.blit(fondo,(0,0))
+
